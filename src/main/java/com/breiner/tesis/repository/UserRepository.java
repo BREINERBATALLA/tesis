943c93fbf96ff30d62548183fbcfe32de11909a9
BREINER BATALLA <breiner.batalla@correounivalle.edu.co>
2023-08-28 19:42:55 -0500

    [ADD] user repository

---
+++ src/main/java/com/breiner/tesis/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.breiner.tesis.repository;
+
+import com.breiner.tesis.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String username);
+}
