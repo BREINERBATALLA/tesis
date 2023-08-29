e1b299fc26af684eea6a9c45f12910ba2fee2765
BREINER BATALLA <breiner.batalla@correounivalle.edu.co>
2023-08-28 19:40:25 -0500

    [ADD] security based on Jwt

---
+++ src/main/java/com/breiner/tesis/service/impl/FileService.java
@@ -0,0 +1,63 @@
+package com.breiner.tesis.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.DeleteObjectResponse;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDate;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class FileService {
+
+    private final S3Client s3Client;
+    @Value("${aws.bucket-name}")
+    private String bucketName;
+
+    public String uploadAudio(MultipartFile file) {
+        String nameFile = LocalDate.now().toString()+file.getOriginalFilename();
+
+        PutObjectRequest request = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(nameFile)
+                .build();
+        try {
+            s3Client.putObject(request, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return nameFile;
+    }
+
+    public void deleteAudio(String fileName) {
+        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                .bucket(bucketName)
+                .key(fileName)
+                .build();
+
+        DeleteObjectResponse deleteObjectResponse = s3Client.deleteObject(deleteObjectRequest);
+
+        /** Verificar si la eliminación fue exitosa
+         if (deleteObjectResponse.sdkHttpResponse().isSuccessful()) {
+         return "El archivo de audio se eliminó correctamente";
+         } else {
+         return "No se pudo eliminar el archivo de audio";
+         }
+         */
+
+    }
+
+
+}
